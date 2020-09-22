614d211f957cec43aad52b4fcc6df10bf2a33e8b
飯田恭子 <iidakyouko@mba.local>
2020-09-22 13:07:55 +0900

    Initial commit

---
+++ src/components/HelloNuxt.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    <h1>Hello,Nuxt!</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- scopedをつけることでこのファイルで作成した単一コンポーネントのみで適用される、という意味 -->
+<style scoped>
+
+</style>
